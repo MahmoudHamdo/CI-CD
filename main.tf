ba7c19ddfbdb6f8cb356e5ef59b58607bc21c3a3
amr <amr.elshawarby10@gmail.com>
2024-03-27 13:28:41 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-access"
+  description = "Allow SSH access from my IP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+data "aws_key_pair" "existing_key_pair" {
+  key_name = "Amrkeypair"
+  
+}
+
+resource "aws_instance" "my-ec2"{
+    ami= var.image
+    instance_type= var.instance_type
+    key_name      = data.aws_key_pair.existing_key_pair.key_name
+    security_groups = [aws_security_group.ssh_access.name] 
+    
+}
+output "instance_ip" {
+  value = aws_instance.my-ec2.public_ip
+}
