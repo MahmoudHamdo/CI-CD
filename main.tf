a7104bb13143e8e32427ed5aea8803089e22f211
amr <amr.elshawarby10@gmail.com>
2024-03-28 20:13:04 +0200

    8-commit

--- main.tf
+++ main.tf
@@ -10,12 +10,6 @@ resource "aws_security_group" "ssh_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
 
   egress {
     from_port   = 0
@@ -33,9 +27,17 @@ resource "aws_instance" "my-ec2"{
     ami= var.image
     instance_type= var.instance_type
     key_name      = data.aws_key_pair.existing_key_pair.key_name
-    security_groups = [aws_security_group.ssh_access.name] 
+    security_groups = [aws_security_group.ssh_access.name]
+     
     
 }
+resource "aws_eip" "my_eip" {
+  vpc      = true  
+}
+resource "aws_eip_association" "my_eip_assoc" {
+  instance_id   = aws_instance.my-ec2.id
+  allocation_id = aws_eip.my_eip.id
+}
 output "instance_ip" {
-  value = aws_instance.my-ec2.public_ip
+  value = aws_eip.my_eip.public_ip
 }
